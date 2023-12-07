fbcb697bdfb5a05433dcfeeba21e479fd332cfc3
Jaewoo Kim <soyou30257@gmail.com>
2023-12-07 22:57:30 +0900

    Studied actuator

---
+++ SpringDataStudy/src/main/java/jp/co/jeus/SpringDataStudy/SpringDataStudyApplication.java
@@ -0,0 +1,13 @@
+package jp.co.jeus.SpringDataStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataStudyApplication.class, args);
+	}
+
+}
