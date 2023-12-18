4d2fff85682194ce9f81f9cf2e69a9a3c97170e0
Jaewoo Kim <soyou30257@gmail.com>
2023-12-18 23:09:00 +0900

    Studied mongodb with gradle

---
+++ SpringMongoStudy/src/test/java/jp/co/jeus/SpringMongoStudy/SpringMongoStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.jeus.SpringMongoStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
